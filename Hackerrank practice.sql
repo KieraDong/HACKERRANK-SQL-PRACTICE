23163909fe1c10c485f6b2be16a43f923f0dd1a9
KieraDong <75791898+KieraDong@users.noreply.github.com>
2023-08-29 00:26:46 +1000

    sql practice

---
+++ Hackerrank practice.sql
@@ -0,0 +1,19 @@
+Hackerrank practice
+
+/* Query the two cities in STATION with the shortest and longest CITY names,
+ as well as their respective lengths (i.e.: number of characters in the name). 
+ If there is more than one smallest or largest city, 
+ choose the one that comes first when ordered alphabetically.
+ */
+
+SELECT TOP 1 WITH TIES
+    CITY,
+    LEN(CITY) AS CITY_LENGTH
+FROM STATION
+ORDER BY CITY_LENGTH ASC, CITY
+
+SELECT TOP 1 WITH TIES
+    CITY,
+    LEN(CITY) AS CITY_LENGTH
+FROM STATION
+ORDER BY CITY_LENGTH DESC, CITY
